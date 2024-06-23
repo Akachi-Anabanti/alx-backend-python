bf5dc57af84e9c0bc755c807ed0cc17c82577d4b
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-23 20:30:42 +0000

    duck typing

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Correctly annotates a function and it's return values'''
+from typing import List, Iterable, Tuple, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''returns a list of tuple of sequence and int'''
+    return [(i, len(i)) for i in lst]
