40634344c20edee48068938f94a290bb22431086
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-23 21:05:30 +0000

    code fix

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' Mypy check and fix'''
+from typing import List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    ''' returns a list of  itmes from lst'''
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
