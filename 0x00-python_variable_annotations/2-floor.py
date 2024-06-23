f0894d6e08ddb58234886e6934179c0dd9f2254a
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-23 17:58:58 +0000

    floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''A script that defines a function that returns the floor
+a float'''
+import math
+
+
+def floor(n: float) -> int:
+    '''returns the floor of n
+    3.14 -> 3
+    '''
+    return math.floor(n)
