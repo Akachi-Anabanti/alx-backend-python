87823b10f0dd7c3c5afa688bfd06a990258ca05c
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-23 18:03:24 +0000

    type casting

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Defines a function that returns the str
+reprensation of a float'''
+
+
+def to_str(n: float) -> str:
+    '''returns the string representation of n'''
+    return str(n)
