c16b8f5fe3cab40d2e8141bf65043020d6916177
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-23 18:39:38 +0000

    sums a list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''A script that defines a function sum_list
+that returns the sumo of a list'''
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    '''returns the sum of input_list'''
+    return sum(input_list)
