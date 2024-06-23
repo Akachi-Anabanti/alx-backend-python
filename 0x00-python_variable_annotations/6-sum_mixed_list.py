55fe8e6b016d7a01a512fed22721da72720a7077
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-23 18:50:57 +0000

    mxd types

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''A script that defines a function sum_mixed_list
+which is a list of mixed float, integers and returns the
+sum'''
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
+    '''returns the sum of mxd_list'''
+    return sum(mxd_list)
