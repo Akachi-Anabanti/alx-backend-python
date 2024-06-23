89e5667e98e2df414aa587267395b172d3fc6750
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-23 17:42:23 +0000

    pycode style fux

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,6 +3,6 @@
 addition operation'''
 
 
-def add (a: float, b: float) -> float:
+def add(a: float, b: float) -> float:
     '''A function that adds to annotated float variables'''
     return a + b
