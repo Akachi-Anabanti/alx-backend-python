9f4b9a2e627e81002286ad098afcc9184e973d03
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-24 22:25:52 +0000

    run random tasks concurrently

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,7 +2,7 @@
 '''Executes an asyncio task without await'''
 from typing import List
 
-task_wait_random = __import__('3-task').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
