065c5de9eed0b97377ffcddabadbd8c1310403c4
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-24 22:17:13 +0000

    fixed type annotation

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,6 +4,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> wait_random:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     ''' returns asyncio task '''
     return asyncio.Task(wait_random(max_delay))
