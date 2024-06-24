63f1461b726c8ee30b1366bd5f4c431cc38bd70e
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-24 14:34:08 +0000

    type annotation correction

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 '''A couroutine that generates random integers'''
-from typing import Iterator, Union
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator() -> Iterator[Union[int, float, None]]:
+async def async_generator() -> Generator[int, None, None]]:
     '''async genrator generates random unifrm numbers'''
     for _ in range(10):
         await asyncio.sleep(1)
