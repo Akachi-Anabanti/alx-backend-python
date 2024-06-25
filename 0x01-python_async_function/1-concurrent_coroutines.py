6c2360c1bdb78aa804fb6ded2149738f409781a0
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-25 13:58:13 +0100

    fixed algorithm

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 '''Executing multiple coroutines'''
 from typing import List
+import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''return a list of floats from delay values'''
-    return [await wait_random(max_delay) for _ in range(n)]
+    wait_times = await asyncio.gather(
+            *tuple(map(lambda _: wait_random(max_delay), range(n)))
+            )
+    return sorted(wait_times)
